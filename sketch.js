d420bf7beebbc98da3acb97ac95d9741d98d4776
DhritiJs <70759231+DhritiJs@users.noreply.github.com>
2020-10-21 16:55:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,48 +1,15 @@
-var ball;
-var database,ballPos,pos;
+var canvas,bgImg;
+var gameState=0;
+var playerCount,database;
+var form,player,game;
 function setup(){
-    createCanvas(500,500);
-    database = firebase.database();
-    ballPos = database.ref('ball/position');
-    ballPos.on("value",readPos,showError);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-
+ canvas = createCanvas(400,400);
+ database = firebase.database();
+game = new Game();
+game.getState();
+game.start();
 }
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
-}
 
-function writePosition(x,y){
-   database.ref('ball/position').set({
-       'x': pos.x+x,
-       'y':pos.y+y
-   })
-
-  //  ball.x = ball.x + x;
-  //  ball.y = ball.y + y;
-}
-
-function readPos(data){
-    pos = data.val();
-    ball.x = pos.x;
-    ball.y = pos.y;
-}
-
-function showError(){
-    console.log("Error");
 }
